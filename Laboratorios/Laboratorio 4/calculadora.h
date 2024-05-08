9c9c6aac075ad76b91d21b9b0698085fddb079eb
ChristianAltamiranda <christian.altamiranda@ucr.ac.cr>
2024-05-07 20:26:30 -0600

    agregando calculadora.h

---
+++ Laboratorios/Laboratorio 4/calculadora.h
@@ -0,0 +1,13 @@
+//Christian Altamiranda Solano (c00261)
+//Declaracion de las funciones
+
+int bitwise_and(int a, int b);
+
+int bitwise_or(int a, int b);
+
+int bitwise_xor(int a, int b);
+
+int bitwise_not(int a);
+
+void print_binary(int decimal);
+
